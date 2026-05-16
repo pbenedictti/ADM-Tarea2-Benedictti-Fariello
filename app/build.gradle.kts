410add1e00db1387933c04ce15267bc6c144bba8
pbenedictti <60271941+pbenedictti@users.noreply.github.com>
2026-05-16 18:30:45 -0300

    Se aplica CI-CD con actions, fastlane y sonarqube

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    id("org.sonarqube") version "7.3.0.8198"
 }
 
 android {
@@ -74,4 +75,14 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "adm-tarea2")
+        property("sonar.projectName", "ADM Tarea2")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.organization", "ADM Tarea2")
+        property("sonar.sources", "app/src/main/java")
+    }
 }
